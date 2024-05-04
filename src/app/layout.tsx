dd8dbcb43013cbaf361ded1d74a66a95db9e91b4
nischal mudennavar <nischalmudennavar@gmail.com>
2024-05-05 00:39:29 +0530

    updated meta

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter,Kode_Mono } from "next/font/google";
 import "./globals.css";
+import gif from '../assets/skull2.gif'
 
 const inter = Inter({ subsets: ["latin"] });
 const kode = Kode_Mono( { subsets: ["latin"] });
@@ -11,12 +12,20 @@ export const metadata: Metadata = {
     'Nischal Mudennavar is technology generalist with a passion for generative ai, software development, frontend engineering, design, physics, anime, and astronomy.',
   keywords:
     'Nischal Mudennavar, UX Engineer, generative ai, software development, frontend engineering, design, physics, anime, astronomy, frontend developer, user experience, UI/UX design, web development, coding, programming, technology, software engineering, graphic design, animation, astrophysics, celestial bodies, Japanese animation, renaissance',
-  authors: [{ name: 'Nischal Mudennavar' }],
-  creator: 'Nischal Mudennavar',
-  publisher: 'Nischal Mudennavar',
   robots: 'index, follow',
- 
-
+  openGraph: {
+    type: 'website',
+    title: 'Nischal Mudennavar',
+    description:
+      'Nischal Mudennavar is technology generalist with a passion for generative ai, software development, frontend engineering, design, physics, anime, and astronomy.',
+    url: 'https://nischal.dev',
+    siteName: 'nischal.dev',
+    images: [
+      {
+        url:gif.src,
+      },
+    ],
+  },
 }
 
 export default function RootLayout({
