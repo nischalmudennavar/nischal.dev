03c82517aae9632f8d2176c959934cb009744767
nischal mudennavar <nischalmudennavar@gmail.com>
2024-05-05 01:12:40 +0530

    updated meta

---
+++ src/app/sitemap.ts
@@ -0,0 +1,14 @@
+import { MetadataRoute } from 'next'
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: 'https://www.nischal.dev',
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 1,
+
+    },
+    
+  ]
+}
