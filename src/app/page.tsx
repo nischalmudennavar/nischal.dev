400a0f2e38ab21bd94b83775c844b1563054cbad
nischal mudennavar <nischalmudennavar@gmail.com>
2024-05-06 22:27:37 +0530

    added elapsed time feature

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,25 @@ import { cn } from '@/lib/utils'
 import { useScramble } from 'use-scramble'
 import Image from 'next/image'
 import gif from '@/assets/skull2.gif'
+import { Slash } from 'lucide-react'
+import { useState, useEffect } from 'react'
+import Link from 'next/link'
 
 export default function Home() {
+  const [elapsedTime, setElapsedTime] = useState('')
+
+  useEffect(() => {
+    const startDate: Date = new Date('2024-05-04')
+    const currentDate: Date = new Date()
+
+    const elapsedMilliseconds = currentDate.getTime() - startDate.getTime()
+
+    const millisecondsInADay = 1000 * 60 * 60 * 24
+    const daysElapsed = Math.floor(elapsedMilliseconds / millisecondsInADay)
+
+    setElapsedTime(daysElapsed)
+  }, [])
+
   const { ref, replay } = useScramble({
     text: 'nischal.dev',
     speed: 1,
@@ -30,7 +47,7 @@ export default function Home() {
     overflow: false,
   })
   const { ref: ref3 } = useScramble({
-    text: 'Crafting a digital masterpiece,\n one line at a time...',
+    text: 'Building one-of-a-kind creative portfolio... ',
     range: [65, 125],
     speed: 1.5,
     tick: 2,
@@ -48,27 +65,45 @@ export default function Home() {
         'flex h-screen flex-col items-center justify-center bg-black  '
       )}
     >
-      <div className='  main-content relative -top-10 flex h-fit flex-col items-center justify-between p-2 text-center lg:w-fit gap-4 sm:w-full sm:flex-wrap sm:text-center lg:flex-row lg:gap-0 lg:text-left'>
+      <div className='  main-content relative -top-10 flex h-fit flex-col items-center justify-between gap-1 p-2 text-center sm:w-full sm:flex-wrap sm:text-center lg:w-fit lg:flex-row lg:gap-0 lg:text-left'>
         <Image
           src={gif}
           alt='gif'
-          width={125}
-          height={125}
+          width={155}
+          height={155}
           className='relative mr-5 '
-          
           priority={true}
         />
-        <div className=' flex h-full flex-col justify-center p-2 lg:items-start sm:items-center  w-fit '>
+        <div className=' flex h-full w-fit flex-col justify-center p-2 sm:items-center  lg:items-start '>
           <p className=' mb-3 text-xl text-white opacity-75' ref={ref2} />
-          <h1
-            ref={ref}
-            className={cn(
-              ' mb-5 text-5xl text-violet-700 transition-all  duration-300 ease-in-out '
-            )}
-            onMouseOver={replay}
-          />
-          <p className='text-sm text-white text-wrap opacity-75 '  ref={ref3} />
+          <Link href='https://www.linkedin.com/in/nischal-mudennavar/'>
+            <h1
+              ref={ref}
+              className={cn(
+                ' mb-5 text-5xl text-violet-700 transition-all  duration-300 ease-in-out '
+              )}
+            />
+          </Link>
+          <div className='flex'>
+            {' '}
+            <p
+              className='text-wrap text-sm text-white opacity-75 '
+              ref={ref3}
+            />
+            <p className='animate-pulse text-wrap text-sm font-bold  text-white  opacity-75 duration-500  '>
+              |{' '}
+            </p>
+          </div>
         </div>
+      </div>
+      <div className=' text-md absolute bottom-10 flex w-[80%] flex-col  gap-2  text-wrap text-center font-bold text-white opacity-75 duration-500 '>
+        <span className='text-2xl text-yellow-400'>
+          {' '}
+          {elapsedTime} Days
+          <br />
+        </span>
+
+        <p className='text-white'>Since I started working on this site 🛠️</p>
       </div>
     </main>
   )
